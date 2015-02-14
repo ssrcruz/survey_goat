a443af4b53c5bdc060ea49c8e96f202008069a1b
Rocky Randalls <rrandalls@gmail.com>
2015-02-14 16:00:22 -0500

    Add options to question creation in survey form #shipit

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :options
   has_many :responses
+
+  accepts_nested_attributes_for :options,
+      :allow_destroy => true,
+      :reject_if     => :all_blank
 end
